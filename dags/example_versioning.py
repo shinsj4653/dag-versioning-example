f65a76de47b0f97e4d80831ef386dd936463d848
Yeonguk <choo121600@gmail.com>
2025-05-18 04:20:40 +0900

    Add example DAG for versioning

---
+++ dags/example_versioning.py
@@ -0,0 +1,35 @@
+from airflow.sdk import dag, task, chain
+from pendulum import datetime
+
+
+@dag(
+    schedule="@daily",
+    start_date=datetime(2025, 5, 1),
+    tags=["example", "dag_versioning"],
+    default_args={
+        "owner": "Airflow Korea",
+        "retries": 3,
+    },
+)
+def dag_versioning_example():
+
+    @task
+    def task_1():
+        print("Hello! task_1")
+
+    @task
+    def task_2():
+        print("Hello! tast_2")
+    
+    @task
+    def task_3():
+        print("Hello! tast_3")
+    
+    chain(
+        task_1(),
+        task_2(),
+        task_3(),
+    )
+
+
+dag_versioning_example()
